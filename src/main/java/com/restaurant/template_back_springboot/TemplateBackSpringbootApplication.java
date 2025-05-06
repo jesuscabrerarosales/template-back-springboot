d3dccbddc00882f54dc57f9a74e5461dfe4bdde1
Jesús E. Cabrera Rosales <jesuscabrerarosales47@gmail.com>
2025-05-06 16:35:52 -0500

    first commit

---
+++ src/main/java/com/restaurant/template_back_springboot/TemplateBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.template_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBackSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/template_back_springboot/TemplateBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.template_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
